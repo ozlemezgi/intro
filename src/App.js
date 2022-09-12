7408aeced19362e1be1de63b18a07b3a1de10705
ozlemezgi <ozlemezgisari@icloud.com>
2022-09-12 22:40:29 +0300

    container row and (reactstrap)

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import React from 'react';
 import CatagoryList from './CatagoryList';
 import Navi from './Navi';
 import ProductList from './ProductList';
+import "bootstrap/dist/css/bootstrap.min.css"
+//import { Container, Row } from 'react-bootstrap';
+import {Container ,Row ,Col} from "reactstrap";
+
 
 function App() {
-  return ( 
+  return (
     // jsx yapısı ; 
-    <div> 
-      <Navi> </Navi>
-      <CatagoryList></CatagoryList>
-      <ProductList></ProductList>
-     {/* <h2>Hello from react </h2>
+    <div>
+      <Container>
+
+        <Row> 
+        <Navi> </Navi>
+        </Row>
+
+        <Row>
+          <Col xs ="3"><CatagoryList></CatagoryList></Col>
+          <Col xs ="9"> <ProductList></ProductList></Col> 
+        </Row>
+
+      </Container>
+      {/* <h2>Hello from react </h2>
       <h3>Deneme</h3>  */}
 
     </div>
-   // <h3>Deneme</h3> //jsx kurallarına aykırı aynı hiyararşide iki ana component oluşturamazsın
-  ); 
+    // <h3>Deneme</h3> //jsx kurallarına aykırı aynı hiyararşide iki ana component oluşturamazsın
+  );
 }
 
 export default App;
--- src/CatagoryList.js
+++ src/CatagoryList.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CatagoryList extends Component {
   render() {
     return (
       <div>
         <h3>Catagory List</h3>
+        <ListGroup>
+          <ListGroupItem>item 1</ListGroupItem>
+          <ListGroupItem>item 2</ListGroupItem>
+          <ListGroupItem>item 3</ListGroupItem>
+          <ListGroupItem>item 4</ListGroupItem>
+        </ListGroup>
       </div>
     )
   }
