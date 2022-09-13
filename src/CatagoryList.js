9c5ecb3c97b36338ccd69cd57e76a7cd5ef8a001
ozlemezgi <ozlemezgisari@icloud.com>
2022-09-13 22:31:22 +0300

    onClick event

--- src/CatagoryList.js
+++ src/CatagoryList.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CatagoryList extends Component {
- state = { categories :[
-      {categoryId:1,categoryName:"Beverages"},
-    {categoryId:2,categoryName:"Condiments"},
-  ]}
-  
-    //state bir componentin datasıdır
-    //props bir componentten ötekine taşınan data yada eventtir.
-  
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiments" },
+    ],
 
+    currentCategory: ""
+  }
+
+  //state bir componentin datasıdır
+  //props bir componentten ötekine taşınan data yada eventtir.
+
+  changeCategory = category => {
+    this.setState({ currentCategory: category.categoryName });
+  } //bu bir fonksiyondur 
   render() {
     return (
       <div>
@@ -19,11 +25,13 @@ export default class CatagoryList extends Component {
         {/* süslü parantez js kodu başlatmak için kullanılır */}
         <ListGroup>
           {
-            this.state.categories.map(category=>(
-              <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            this.state.categories.map(category => (
+              <ListGroupItem onClick={() => this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
             ))
+            //setstate statedeki herhangi bir nesnenin eğiştirlmesini sağlar
           }
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     )
   }
