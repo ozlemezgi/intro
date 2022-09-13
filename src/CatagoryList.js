f337a4debb3e2f562d6df408e95db6deb617ca95
ozlemezgi <ozlemezgisari@icloud.com>
2022-09-13 22:12:09 +0300

    state

--- src/CatagoryList.js
+++ src/CatagoryList.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CatagoryList extends Component {
-  constructor(props){
-    super(props);
-    state:{}
-  }
+ state = { categories :[
+      {categoryId:1,categoryName:"Beverages"},
+    {categoryId:2,categoryName:"Condiments"},
+  ]}
+  
+    //state bir componentin datasıdır
+    //props bir componentten ötekine taşınan data yada eventtir.
+  
 
   render() {
     return (
       <div>
-        <h3>{this.props.info.title}</h3> 
+        <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
         {/* süslü parantez js kodu başlatmak için kullanılır */}
         <ListGroup>
-          <ListGroupItem>item 1</ListGroupItem>
-          <ListGroupItem>item 2</ListGroupItem>
-          <ListGroupItem>item 3</ListGroupItem>
-          <ListGroupItem>item 4</ListGroupItem>
+          {
+            this.state.categories.map(category=>(
+              <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            ))
+          }
         </ListGroup>
       </div>
     )
